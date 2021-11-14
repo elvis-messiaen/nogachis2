574c2cd62749640ba930ec16c173e51ea4f7958f
Elvis Messiaen <elvism72@gmail.com>
2021-11-14 16:42:02 +0100

    first commit

---
+++ src/service/auth.service.ts
@@ -0,0 +1,21 @@
+import { rejects } from "assert";
+import { resolve } from "dns";
+
+export class AuthService [
+    isAuth = false;
+
+    signIn () {
+        return new Promise (
+            executor; (resolve,rejects) => {
+                setTimeout (
+                    handler: () => {
+                        this.isAuth = true;
+                        resolve(true);
+                    }, 2000
+                );
+            }
+        );
+    }
+    
+
+]
